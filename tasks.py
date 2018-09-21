75bfde8c77ff454cd5ac301629189142c3b927f3
georgeli <george.li@gliacloud.com>
2018-09-21 23:06:43 +0800

    add copy

--- tasks.py
+++ tasks.py
@@ -7,9 +7,9 @@ app = Celery('tasks', broker='sqla+sqlite:///celerydb.sqlite', backend='db+sqlit
 @app.task
 def to_mp4(m3u8_url, output_path, st=0, t=None):
     if t:
-        cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -ss {st} -t {t} {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st, t=t).split()
+        cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -vcodec copy -ss {st} -t {t} {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st, t=t).split()
     elif st:
-        cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -ss {st} {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st).split()
+        cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -vcodec copy -ss {st} {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st).split()
     else:
         cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -vcodec copy {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st).split()
 
