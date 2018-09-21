a31c4c48a70db5ef4243173a2abadeabd39132b5
georgeli <george.li@gliacloud.com>
2018-09-21 23:05:19 +0800

    add copy

--- tasks.py
+++ tasks.py
@@ -8,8 +8,10 @@ app = Celery('tasks', broker='sqla+sqlite:///celerydb.sqlite', backend='db+sqlit
 def to_mp4(m3u8_url, output_path, st=0, t=None):
     if t:
         cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -ss {st} -t {t} {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st, t=t).split()
-    else:
+    elif st:
         cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -ss {st} {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st).split()
+    else:
+        cmd = 'ffmpeg -loglevel panic -protocol_whitelist file,http,https,tcp,tls -i {m3u8_path} -vcodec copy {output_path}'.format(m3u8_path=m3u8_url, output_path=output_path, st=st).split()
 
     proc = subprocess.Popen(cmd)
     proc.wait()
